9b280397d0c739330d9521368e8fda06c9f7aa85
bisayangmarino <orillanedanikko@outlook.com>
2022-02-28 16:04:39 +0800

    Done exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(input) {
+  let answer = input - 32;
+  answer *= 5 / 9;
+  return Math.round(answer * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(input) {
+  let answer = input * 9 / 5 + 32;
+  return Math.round(answer * 10) / 10;
 };
-
 // Do not edit below this line
 module.exports = {
   ftoc,
